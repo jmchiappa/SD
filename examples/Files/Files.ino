316ae89e1663de80f94679ea131a94d1d8a03065
jean-marc <jmc.chiappa@gmail.com>
2024-09-28 19:27:12 +0200

    update SD library with handler for another SPI peripheral

---
+++ examples/Files/Files.ino
@@ -0,0 +1,57 @@
+
+#include <SPI.h>
+#include <SD.h>
+
+#define SD_MISO  D29
+#define SD_MOSI  D17
+#define SD_SCK   D16
+#define SD_CSn   D30
+
+// SPI3 of NUCLEO L476RG (without SSn)
+
+SPIClass mySPI(SD_MOSI,SD_MISO,SD_SCK);
+
+uint32_t FULL_SPEED  = 0; // 50MHz
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(mySPI,FULL_SPEED,SD_CSn)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  if (dataFile) {
+    for(uint i=0;i<255;i++) {
+      dataFile.println("coordonnées : toto");
+    }
+    dataFile.close();
+    Serial.println("batch written");
+}
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ examples/anotherSPI/anotherSPI.ino
@@ -0,0 +1,81 @@
+/*
+  SD card datalogger
+
+  This example shows how to log data from three analog sensors
+  to an SD card using the SD library.
+
+  created  24 Nov 2010
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+#define SD_MISO D29
+#define SD_MOSI D17
+#define SD_SCK  D16
+#define SD_CSn  D30
+
+SPIClass mySPI(SD_MISO,SD_MOSI,SD_SCK,SD_CSn);
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(mySPI)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  // make a string for assembling the data to log:
+  String dataString = "";
+
+  // read three sensors and append to the string:
+  for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 2) {
+      dataString += ",";
+    }
+  }
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
+
+
+
+
+
+
+
+
